d74003e150c97d279bff0f66d613da73d4075282
boyx13 <flippi1313@gmail.com>
2020-04-13 15:23:46 +0200

    Adjust Mainclass

--- src/main/java/shit/industrialtech/IndustrialTech.java
+++ src/main/java/shit/industrialtech/IndustrialTech.java
@@ -1,8 +1,27 @@
 package shit.industrialtech;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(value = "industrialtech")
 public class IndustrialTech {
+	
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public IndustrialTech() {
+		
+	}
+	
+	private void setup(final FMLCommonSetupEvent event) {
+		
+	}
+	
+	private void doClientStuff(final FMLClientSetupEvent event) {
+		
+	}
 	
 }
